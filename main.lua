5c49d09c895b1deef3e7ce8a3d2acf789be5f45a
Ubuntu <ubuntu@ip-172-31-47-195.us-east-2.compute.internal>
2021-09-18 22:31:38 +0000

    difficulty

--- main.lua
+++ main.lua
@@ -1,9 +1,9 @@
-local alpha = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p", "q", "r", "s", "t", "u", "v"}
+local alpha = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v"}
 
 local path = "/home/ubuntu/jklmcheat/"
 local used_words = {}
 
-
+ct=0
 
 function bool_to_text(bool)
 	if bool then return "true" end
@@ -20,6 +20,48 @@ function copy(obj, seen)
 end
 
 
+
+local alpha_pos = {
+	["q"] = {1, 3},
+	["w"] = {2, 3},
+	["e"] = {3, 3},
+	["r"] = {4, 3},
+	["t"] = {5, 3},
+	["y"] = {6, 3},
+	["u"] = {7, 3},
+	["i"] = {8, 3},
+	["o"] = {9, 3},
+	["p"] = {10, 3},
+	["a"] = {1, 2},
+	["s"] = {2, 2},
+	["d"] = {3, 2},
+	["f"] = {4, 2},
+	["g"] = {5, 2},
+	["h"] = {6, 2},
+	["j"] = {7, 2},
+	["k"] = {8, 2},
+	["l"] = {9, 2},
+	["z"] = {1, 1},
+	["x"] = {2, 1},
+	["c"] = {3, 1},
+	["v"] = {4, 1},
+	["b"] = {5, 1},
+	["m"] = {6, 1},
+	["n"] = {7, 1}
+}
+
+local function calculate_difficulty(word)
+	local distance = 0
+	local current_letter, prev_letter
+	for i = 2, #word do
+		current_letter = string.sub(word, i, i)
+		prev_letter = string.sub(word, i - 1, i - 1)
+		distance = distance + math.sqrt((alpha_pos[current_letter][1] - alpha_pos[prev_letter][1]) ^ 2 + (alpha_pos[current_letter][2] - alpha_pos[prev_letter][2]) ^ 2)
+	end
+
+	return distance
+end
+
 function calculate_diversity(word, working_alpha)
 	current_alpha = copy(working_alpha)
 	local diversity = 0
@@ -29,6 +71,7 @@ function calculate_diversity(word, working_alpha)
 		for _, v in pairs(working_alpha) do
 			if current_alpha[letter] then
 				diversity = diversity + 1
+				ct=ct+1
 				current_alpha[letter] = false
 			end
 		end
@@ -43,7 +86,8 @@ function best_word(pattern, tAlpha)
 	local word = f:read()
 	local best = ""
 	local best_diversity = -1
-	local diversity, best_alpha, working_alpha
+	local best_difficulty = 999999
+	local diversity, best_alpha, working_alpha, difficulty
 	while word do
 		for _, v in pairs(used_words) do
 			if v == word then
@@ -52,13 +96,14 @@ function best_word(pattern, tAlpha)
 		end
 
 		diversity, working_alpha = calculate_diversity(word, constanttest)
-
-		if diversity > best_diversity then
+		difficulty = calculate_difficulty(word)
+		if diversity > best_diversity or (diversity == best_diversity and difficulty < best_difficulty)then
 
 			best = word
 			best_diversity = diversity
 			best_alpha = working_alpha
-			--print("new best: ".. best.. "\t" .. best_diversity)
+			best_difficulty = difficulty
+			--print("new best: ".. best.. "\t" .. best_diversity .. "\t" .. best_difficulty)
 		end
 
 		::read_line::
@@ -72,32 +117,38 @@ end
 
 
 
-local current_alpha = {}
-for _, v in pairs(alpha) do
-	current_alpha[v] = true
-end
-
 local reset_alpha, pattern, word, working_alpha
-while true do
-	reset_alpha = false
-	for _, v in pairs(current_alpha) do
-		reset_alpha = reset_alpha or v
-		if v then break end
-	end
-	if not reset_alpha then
-		for _, v in pairs(alpha) do
-			current_alpha[v] = true
-		end
-		print("gained health")
-	end
-	io.write("Pattern: ")
-	pattern = io.read()
-	word, working_alpha = best_word(pattern, current_alpha)
-	local diversity, _ = calculate_diversity(word, current_alpha)
+local current_alpha = {}
 
-	print(word .. " \t" .. diversity)
-	io.write("did you die? (y/n) ")
-	if string.lower(io.read()) ~= "y" then
-		current_alpha = working_alpha
+function main()
+	for _, v in pairs(alpha) do
+		current_alpha[v] = true
+	end
+
+	while true do
+		reset_alpha = false
+		for _, v in pairs(current_alpha) do
+			reset_alpha = reset_alpha or v
+			if v then break end
+		end
+		if not reset_alpha then
+			for _, v in pairs(alpha) do
+				current_alpha[v] = true
+			end
+			print("gained health")
+		end
+		io.write("Pattern: ")
+		pattern = io.read()
+		word, working_alpha = best_word(pattern, current_alpha)
+		local diversity, _ = calculate_diversity(word, current_alpha)
+
+		print(word .. " \t" .. diversity)
+		io.write("did you die? (y/n) ")
+		if string.lower(io.read()) ~= "y" then
+			current_alpha = working_alpha
+		end
 	end
 end
+
+main()
+
